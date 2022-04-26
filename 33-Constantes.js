a2e9fcc2b6fd19ea677c5462f038aaeaaadc0157
RVSISTEMA <rvsistema@gmail.com>
2022-04-26 16:16:44 -0300

    Modulos; clausulas module nomodule export import destructuracion etc

--- 33-Constantes.js
+++ 33-Constantes.js
@@ -1,1 +1,15 @@
 export const PI = Math.PI;
+
+export let usuario = "jon";
+export let password = "qwerty";
+
+export function saludar(){
+  console.log("Hola modulos +ES6");
+};
+
+export default class Saludar{
+  constructor(){
+    console.log("Hola Modulos");
+  }
+};
+
--- 33-Modulos.js
+++ 33-Modulos.js
@@ -1,7 +1,10 @@
-import{
-PI
-} from "./33-Constantes.js";
-
+import Saludar,{saludar,PI, usuario} from "./33-Constantes.js";
+import {aritmetica as ari} from "./33-Aritmetica.js";
 
 console.log("Archivo Modulos.js");
-console.log(PI);
+console.log(PI, usuario);
+console.log(ari.sumar(3,4));
+console.log(ari.restar(3,4));
+
+
+const saludo = new Saludar;
