ff361292ffa5cc58b410f1851e69af15eac1eace
RVSISTEMA <rvsistema@gmail.com>
2022-05-03 17:37:25 -0300

    Integrando separadores y jslint.

--- 36-Problemas.js
+++ 36-Problemas.js
@@ -2,4 +2,16 @@
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
 
-const numeroAleatorio=()=>Math.random(601-500)*500;
+
+
+const numeroAleatorio=(min,max)=>Math.round(Math.random()*(max-min)+min);
+
+const evaluarCapicua=(strNum)=>{
+  if(parseInt(strNum)>=0) console.log("No Ingresaste un numero");
+
+};
+
+let limiteInferior = parseInt(window.prompt("Ingrese el limite inferior para generar aleatorio:"));
+let limiteSuperior = parseInt(window.prompt("Ingrese el limite Superior para generar aleatorio:"));
+console.info(numeroAleatorio(limiteInferior,limiteSuperior));
+let numeroCapicua = window.prompt("Ingrese el numero para evaluar:");
