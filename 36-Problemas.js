ab86594113ca27979ab703c83d0fee5542e270d0
rvsistema <rvsistema@gmail.com>
2022-05-08 17:59:50 -0300

    Nos vamos salteando algunas validaciones.

--- 36-Problemas.js
+++ 36-Problemas.js
@@ -2,13 +2,13 @@
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
 
-
-
 const numeroAleatorio=(min,max)=>Math.round(Math.random()*(max-min)+min);
 
 const evaluarCapicua=(strNum)=>{
   if(parseInt(strNum)>=0) console.log("No Ingresaste un numero");
-
+  if(strNum.lenght()=1) return parseInt(strNum);
+  let strNumRev=strNum.split().reverse().join();
+  if (strNum===strNumRev) console.log ("En numero "+ strNum +" es capicúa")
 };
 
 let limiteInferior = parseInt(window.prompt("Ingrese el limite inferior para generar aleatorio:"));
