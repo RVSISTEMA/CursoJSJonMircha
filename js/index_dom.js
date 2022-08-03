bd34eb2768c97c9e4d69bf016dd9b8fe56d6156d
RVSISTEMA <rvsistema@gmail.com>
2022-08-03 11:45:41 -0300

    por fin css

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,7 +1,7 @@
 
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 const d=document;
-
+alert("Indexdom");
 d.addEventListener("DOMContentLoaded",(e)=>{
   hamburgerMenu(".panel-btn",".panel");
 });
